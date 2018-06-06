0a4e7178d0d54d27114c2855523cd2fd9d70d8a2
AngusWu <anguswu@simbo.com.tw>
2018-06-07 01:36:57 +0800

    very ugly layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div class="d-flex bg-primary py-3">
+      <div class="container justify-content-between d-flex todo-nav">
+        <a href="#" class="active">My Task</a>
+        <a href="#">In Progress</a>
+        <a href="#">Completed</a>
+      </div>
+    </div>
+    <div class="container mt-4">
+      <div>
+        <input type="text" class="form-control form-control-lg"/>
+      </div>
+
+      <div class="mt-4">
+        <div  class="todo-item">
+          <div class="todo-header"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+$darn-primary: #00408B;
+$light-warning: #FFF2DC;
+.todo-nav {
+  a {
+    color: $darn-primary;
+    font-size: 1.5rem;
+    text-decoration: none;
+    display: block;
+    padding-top: 24px;
+    padding-bottom: 24px;
+    flex: 1;
+    text-align: center;
+    &:active, &:hover, &.active{
+      box-shadow: 0 -5px $darn-primary inset;
+    }
+    &.active {
+      color: white;
+    }
+  }
+}
+
+.todo-item {
+  display: flex;
+  flex-direction: column;
+  background-color: $light-warning;
+  border-radius: 3px;
+  padding: 24px;
+  .todo-header {
+    display: flex;
+  }
+
+  .todo-title {
+    flex: 1;
+    font-size: 1.5rem;
+  }
 }
 </style>
