0be7a7b16ec5438d03b37cbd13f743b90cb0e583
AngusWu <anguswu@simbo.com.tw>
2018-06-08 19:47:34 +0800

    modify todo-nav

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div class="d-flex bg-primary py-3">
+    <div class="bg-primary">
       <div class="container justify-content-between d-flex todo-nav">
         <a href="#" class="active">My Task</a>
         <a href="#">In Progress</a>
@@ -13,7 +13,7 @@
       </div>
 
       <div class="mt-4">
-        <div  class="todo-item">
+        <div class="todo-item">
           <div class="todo-header"></div>
         </div>
       </div>
@@ -28,40 +28,38 @@ export default {
 </script>
 
 <style lang="scss">
-$darn-primary: #00408B;
-$light-warning: #FFF2DC;
-.todo-nav {
-  a {
-    color: $darn-primary;
-    font-size: 1.5rem;
-    text-decoration: none;
-    display: block;
-    padding-top: 24px;
-    padding-bottom: 24px;
-    flex: 1;
-    text-align: center;
-    &:active, &:hover, &.active{
-      box-shadow: 0 -5px $darn-primary inset;
-    }
-    &.active {
-      color: white;
+  $darn-primary: #00408B;
+  $light-warning: #FFF2DC;
+  .todo-nav {
+    a {
+      color: $darn-primary;
+      font-size: 1.5rem;
+      text-decoration: none;
+      display: block;
+      padding-top: 20px;
+      padding-bottom: 20px;
+      flex: 1;
+      text-align: center;
+      &:active, &:hover, &.active {
+        color: white;
+        box-shadow: 0 -5px $darn-primary inset;
+      }
     }
   }
-}
 
-.todo-item {
-  display: flex;
-  flex-direction: column;
-  background-color: $light-warning;
-  border-radius: 3px;
-  padding: 24px;
-  .todo-header {
+  .todo-item {
     display: flex;
-  }
+    flex-direction: column;
+    background-color: $light-warning;
+    border-radius: 3px;
+    padding: 24px;
+    .todo-header {
+      display: flex;
+    }
 
-  .todo-title {
-    flex: 1;
-    font-size: 1.5rem;
+    .todo-title {
+      flex: 1;
+      font-size: 1.5rem;
+    }
   }
-}
 </style>
